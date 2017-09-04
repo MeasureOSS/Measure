25b1a42e1eba2721aaa10441c034d1ef431866b8
Stuart Langridge <sil@kryogenix.org>
2017-09-04 17:07:19 +0100

    Add link to big numbers

--- widgets/repo/10_prCount.js
+++ widgets/repo/10_prCount.js
@@ -20,6 +20,12 @@ module.exports = function(options, callback) {
         return options.db.pull_request.count({state: "closed", closed_at: {$gt: oneMonthAgo}});
     }).then(closedSinceCount => {
         counts.closedSinceCount = closedSinceCount;
+        return options.db.issue.find({}, {repository_url:1}).limit(1).toArray();
+    }).then(firstIssue => {;
+        var link;
+        if (firstIssue.length > 0) {
+            link = firstIssue[0].repository_url.replace('api.github.com/repos', 'github.com') + '/pulls';
+        }
         let openAMonthAgoCount = counts.openThenCount + counts.closedSinceCount;
         let diff = ((openAMonthAgoCount < counts.openNowCount) ? "+" : "") + 
             (counts.openNowCount - openAMonthAgoCount);
@@ -28,7 +34,8 @@ module.exports = function(options, callback) {
             bignumber: counts.openNowCount,
             unit: "Pull Requests",
             changename: moment().format("MMMM"),
-            changeamount: diff
+            changeamount: diff,
+            link: link
         }
         options.templates.bignumber(result, callback);
     }).catch(e => { callback(e); });
--- widgets/repo/20_issueCount.js
+++ widgets/repo/20_issueCount.js
@@ -20,6 +20,13 @@ module.exports = function(options, callback) {
         return options.db.issue.count({state: "closed", pull_request: null, closed_at: {$gt: oneMonthAgo}});
     }).then(closedSinceCount => {
         counts.closedSinceCount = closedSinceCount;
+        return options.db.issue.find({}, {repository_url:1}).limit(1).toArray();
+    }).then(firstIssue => {;
+        var link;
+        if (firstIssue.length > 0) {
+            link = firstIssue[0].repository_url.replace('api.github.com/repos', 'github.com') + '/issues';
+        }
+
         let openAMonthAgoCount = counts.openThenCount + counts.closedSinceCount;
         let diff = ((openAMonthAgoCount < counts.openNowCount) ? "+" : "") + 
             (counts.openNowCount - openAMonthAgoCount);
@@ -28,7 +35,8 @@ module.exports = function(options, callback) {
             bignumber: counts.openNowCount,
             unit: "issues",
             changename: moment().format("MMMM"),
-            changeamount: diff
+            changeamount: diff,
+            link: link
         }
         options.templates.bignumber(result, callback);
     }).catch(e => { callback(e); });

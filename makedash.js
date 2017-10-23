9703142871f06128d07fe221cb764905e063da9d
Stuart Langridge <sil@kryogenix.org>
2017-10-23 19:33:24 +0100

    Add the reporting infrastructure, and one very simple example report

--- makedash.js
+++ makedash.js
@@ -42,7 +42,7 @@ function loadTemplates() {
     */
     const TEMPLATES_LIST = ["list", "bignumber", "graph", "dashboard", "front", 
         "table", "dl", "notes", "orgs", "redirect", "fromto", "repositories",
-        "organizations"];
+        "organizations", "report", "reportlist"];
 
     return new Promise((resolve, reject) => {
         /*
@@ -1053,6 +1053,121 @@ function indexPages(options) {
     })
 }
 
+function findReportFiles() {
+    return new Promise((resolve, reject) => {
+        var pth = path.join(__dirname, "reports");
+        glob(pth + "/*.js", function(err, files) {
+            if (err) { return reject(err); }
+            return resolve(files);
+        });
+    });
+}
+function loadReportModule(fn, options) {
+    return new Promise((resolve, reject) => {
+        var mod = null;
+        try {
+            var parts = fn.split("/");
+            var name = parts[parts.length-1].replace(/\.js$/,'');
+            mod = require(fn);
+            return resolve({
+                module: mod,
+                filename: fn,
+                name: name
+            });
+        } catch(e) {
+            console.warn("Skipping ill-formed report module", fn, e);
+            return resolve(null);
+        }
+    });
+}
+function executeReportModule(mod, options, in_params) {
+    return new Promise((resolve, reject) => {
+        try {
+            mod.module(in_params, (err, result) => {
+                if (err) {
+                    console.warn("Skipping report which threw an error", err, mod.filename);
+                    return resolve(null);
+                }
+                return resolve({result: result, module: mod});
+            });
+        } catch(e) {
+            console.warn("Skipping report which threw an error", e, mod.filename);
+            return resolve(null);
+        }
+    });
+}
+function writeReport(result, options) {
+    return new Promise((resolve, reject) => {
+        const outputFile = path.join(options.userConfig.output_directory, "reports", result.module.name + ".html");
+        var tmplvars = Object.assign(result.result, {isReport: true});
+        options.templates.report(tmplvars, (err, output) => {
+            if (err) return reject(err);
+            output = fixOutputLinks(output, outputFile, options);
+            fs.writeFile(outputFile, output, {encoding: "utf8"}, err => {
+                if (err) {
+                    return reject(NICE_ERRORS.COULD_NOT_WRITE_OUTPUT(err, outputFile));
+                }
+                return resolve({
+                    slug: result.module.name,
+                    title: result.result.title
+                });
+            })
+        });
+    });
+}
+function writeReportList(reports, options) {
+    return new Promise((resolve, reject) => {
+        const outputFile = path.join(options.userConfig.output_directory, "reports.html");
+        options.templates.reportlist({reports:reports, isReport: true}, (err, output) => {
+            if (err) return reject(err);
+            output = fixOutputLinks(output, outputFile, options);
+            fs.writeFile(outputFile, output, {encoding: "utf8"}, err => {
+                if (err) {
+                    return reject(NICE_ERRORS.COULD_NOT_WRITE_OUTPUT(err, outputFile));
+                }
+                return resolve();
+            })
+        });
+    });
+}
+
+function reports(options) {
+    /* Reports live in reports/*.js, are called with the same parameters as widgets,
+       and are expected to pass {title: "Some Title", html: "<some><html>"} to their
+       callback, which will then be created as a report named reports/<reportModuleName.html> */
+    return new Promise((resolve, reject) => {
+        options.db.collections((err, colls) => {
+            if (err) { return reject(err); }
+            var colldict = {};
+            colls.forEach(c => { colldict[c.collectionName] = c; })
+            var in_params = {
+                db: colldict, 
+                templates: options.templates, 
+                url: url_lookup,
+                config: options.userConfig,
+                org2People: options.org2People,
+                COLORS: COLORS
+            };
+            try {
+                fs.mkdirSync(path.join(options.userConfig.output_directory, "reports"));
+            } catch(e) {
+                if (e.code != "EEXIST") { return reject(e); }
+            }
+            return findReportFiles(options)
+                .then(files => Promise.all(files.map(f => loadReportModule(f, options))))
+                .then(modules => Promise.all(modules.filter(m=>!!m).map(m => executeReportModule(m, options, in_params))))
+                .then(results => Promise.all(results.filter(r=>!!r).map(r => writeReport(r, options))))
+                .then(reports => writeReportList(reports, options))
+                .then(() => {
+                    return resolve(options);
+                })
+                .catch(e => {
+                    return reject(e);
+                })
+        });
+    });
+}
+
 function frontPage(options) {
     return new Promise((resolve, reject) => {
         let links = options.userConfig.github_repositories.map(op => {
@@ -1147,6 +1262,7 @@ loadTemplates()
     .then(dashboardForEachRepo)
     .then(dashboardForEachContributor)
     .then(dashboardForEachOrg)
+    .then(reports)
     .then(frontPage)
     .then(indexPages)
     .then(leave)
---
+++ reports/reposByCreationDate.js
@@ -0,0 +1,13 @@
+var moment = require("moment");
+module.exports = function(options, callback) {
+    options.db.repo.find({}, {full_name: 1, created_at: 1}).sort({created_at: 1}).toArray().then(repos => {
+        var trs = repos.map(r => {
+            return "<tr><td>" + escape(r.full_name) + "</td><td>" + moment(r.created_at).format("YYYY-MM-DD") + "</td></tr>";
+        })
+        var html = "<table>" + trs.join("\n") + "</table>";
+        return callback(null, {
+            title: "Repositories by Creation Date",
+            html: html
+        })
+    })
+}

5a9c7ab720dae379ca1c53efbc1281563d5e3168
Stuart Langridge <sil@kryogenix.org>
2017-11-08 10:33:34 +0000

    Organisations were being filtered by new-style org2People {login:whatever} dicts, not just by logins, so there were no results. Fix that.

--- widgets/org/usersInThisOrg.js
+++ widgets/org/usersInThisOrg.js
@@ -1,8 +1,8 @@
 module.exports = function(options, callback) {
     let result = {
         title: "This organization",
-        list: options.limitedTo.sort((a,b) => { return a.login.localeCompare(b.login); }).map(u => { 
-            return {html: '<a href="' + options.url("contributor", u.login) + '">' + u.login + '</a>'}; 
+        list: options.limitedTo.sort().map(u => { 
+            return {html: '<a href="' + options.url("contributor", u) + '">' + u + '</a>'}; 
         })
     }
     options.templates.list(result, callback);

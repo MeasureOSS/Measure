a6b7667007444459ee6033f61b7a86ac2d603327
Stuart Langridge <sil@kryogenix.org>
2018-03-03 15:10:36 +0000

    Try/catch handling in the error handler

--- makedash.js
+++ makedash.js
@@ -291,30 +291,40 @@ loads.loadTemplates()
     .then(pages.searchPage)
     .then(leave)
     .catch(e => {
-        if (db) db.close();
-        if (e.isNiceError) {
-            console.error("Problem message:")
-            console.error(e.message);
-        } else {
-            require("stacktrace-js").fromError(e)
-                .then(function(frames) {
-                    var lineno = 0, sourcefile = "(an unknown file)";
-                    if (frames.length) {
-                        lineno = frames[0].lineNumber + ":" + frames[0].columnNumber;
-                        sourcefile = path.relative(__dirname, frames[0].fileName);
-                    }
-                    console.error("\nAn internal error has occurred: " + e.message + ".\n" +
-                        "Internal errors are by definition a bug in Measure, and should be reported.\n" +
-                        "The error was detected at line " + lineno + " of '" + sourcefile + "'.\n" + 
-                        "This should never happen, and I am halting in abject failure.");
-                })
-                .catch(function(ste) {
-                    console.error("\nAn internal error has occurred: " + e.message + ".\n" +
-                        "Internal errors are by definition a bug in Measure, and should be reported.\n" +
-                        "Additionally, while handling that error another error occurred: " + 
-                            ste.message + ".\n" +
-                        "The original error may have been " + e.stack.split("\n")[1].trim() + "\n" +
-                        "This should never happen, and I am halting in abject failure.");
-                })
+        try {
+            faoi.error;
+            if (db) db.close();
+            if (e.isNiceError) {
+                console.error("Problem message:")
+                console.error(e.message);
+            } else {
+                require("stacktrace-js").fromError(e)
+                    .then(function(frames) {
+                        var lineno = 0, sourcefile = "(an unknown file)";
+                        if (frames.length) {
+                            lineno = frames[0].lineNumber + ":" + frames[0].columnNumber;
+                            sourcefile = path.relative(__dirname, frames[0].fileName);
+                        }
+                        console.error("\nAn internal error has occurred: " + e.message + ".\n" +
+                            "Internal errors are by definition a bug in Measure, and should be reported.\n" +
+                            "The error was detected at line " + lineno + " of '" + sourcefile + "'.\n" + 
+                            "This should never happen, and I am halting in abject failure.");
+                    })
+                    .catch(function(ste) {
+                        console.error("\nAn internal error has occurred: " + e.message + ".\n" +
+                            "Internal errors are by definition a bug in Measure, and should be reported.\n" +
+                            "Additionally, while handling that error another error occurred: " + 
+                                ste.message + ".\n" +
+                            "The original error may have been " + e.stack.split("\n")[1].trim() + "\n" +
+                            "This should never happen, and I am halting in abject failure.");
+                    })
+            }
+        } catch(ste) {
+            console.error("\nAn internal error has occurred: " + e.message + ".\n" +
+                "Internal errors are by definition a bug in Measure, and should be reported.\n" +
+                "Additionally, while handling that error another error occurred: " + 
+                    ste.message + ".\n" +
+                "The original error may have been " + e.stack.split("\n")[1].trim() + "\n" +
+                "This should never happen, and I am halting in abject failure.");
         }
     });
